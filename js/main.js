a70d957dc5e98cbb9d3d0fa57db1ccf7c8fe6bc3
Vivek <vivekthhl@gmail.com>
2021-08-14 18:14:25 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,169 @@
+import {Stopwatch} from "./stopwatch.js"
+
+var sw;
+var timerId;
+var DateToWorkingHoursMap;
+
+function getLengthAdjustedTimeUnitString(timeUnitStr)
+{
+    if(1 == timeUnitStr.length)
+    {
+        return "0" + timeUnitStr;
+    }
+    return timeUnitStr;
+}
+
+function updateClock()
+{
+    var hoursStr = (sw.getHours()).toString();
+    var minutesStr = (sw.getMinutes()).toString();
+    var secondsStr = (sw.getSeconds()).toString();
+
+    
+    document.getElementById("hours").innerHTML = getLengthAdjustedTimeUnitString(hoursStr); ;
+    document.getElementById("minutes").innerHTML = getLengthAdjustedTimeUnitString(minutesStr);
+    document.getElementById("seconds").innerHTML = getLengthAdjustedTimeUnitString(secondsStr);
+
+    //console.log(hoursStr + " " + minutesStr + " " + secondsStr);
+    
+    var nowDate = new Date(); 
+    var date = (getLengthAdjustedTimeUnitString((nowDate.getFullYear()).toString()) + '/' +
+                getLengthAdjustedTimeUnitString((nowDate.getMonth()+1).toString()) + '/' +
+                getLengthAdjustedTimeUnitString((nowDate.getDate()).toString())); 
+
+    console.log("Value before store: " + sw.getTimeAsString());
+    DateToWorkingHoursMap.set(date, sw.getTimeAsString());
+
+    for (let [key, value] of DateToWorkingHoursMap) {
+        localStorage.setItem(key, value);
+    }      
+}
+
+export function init(){
+    loadMap();
+
+    sw = new Stopwatch();
+    
+    if(!(typeof DateToWorkingHoursMap !== 'undefined')){
+        DateToWorkingHoursMap = new Map();
+        return;
+    }
+    if(DateToWorkingHoursMap.size == 0){
+        return;
+    }
+
+    console.log("here");
+
+    //RETRIEVE PREVIOUS DATA;
+    var outputDiv = document.getElementById("log");
+    var outputStr = "";
+    
+    var nowDate = new Date(); 
+    var date = (getLengthAdjustedTimeUnitString((nowDate.getFullYear()).toString()) + '/' +
+                getLengthAdjustedTimeUnitString((nowDate.getMonth()+1).toString()) + '/' +
+                getLengthAdjustedTimeUnitString((nowDate.getDate()).toString()));     
+
+    var workingHours = DateToWorkingHoursMap.get(date);
+    var workingHoursSplitBySpace = workingHours.split(":");
+    var hoursStr = workingHoursSplitBySpace[0];
+    var minutesStr = workingHoursSplitBySpace[1];
+    var secondsStr = workingHoursSplitBySpace[2];
+
+    document.getElementById("hours").innerHTML = getLengthAdjustedTimeUnitString(hoursStr); ;
+    document.getElementById("minutes").innerHTML = getLengthAdjustedTimeUnitString(minutesStr);
+    document.getElementById("seconds").innerHTML = getLengthAdjustedTimeUnitString(secondsStr);
+
+    //update internal clock
+    var timeElapsedInSeconds = (parseInt(hoursStr)*3600 +
+                                parseInt(minutesStr)*60 +
+                                parseInt(secondsStr));
+
+    console.log("timeElapsedInSeconds" + timeElapsedInSeconds);
+    
+    sw.setTime(timeElapsedInSeconds);
+    
+    console.log("Fetched Time:" + hoursStr + " " + minutesStr + " " + secondsStr);
+    
+    outputStr = getDateWorkingHoursTable();
+    outputDiv.innerHTML = outputStr;    
+}
+
+export function buttonClick(){
+
+    if(document.getElementById("button").innerHTML == "Start")
+    {
+        document.getElementById("button").innerHTML = "Stop";
+        start();
+    }
+    else if(document.getElementById("button").innerHTML == "Stop")
+    {
+        document.getElementById("button").innerHTML = "Start";        
+        stop();
+    }
+
+    for (let [key, value] of DateToWorkingHoursMap) {
+        localStorage.setItem(key, value);
+    }       
+}
+
+function start(){
+    sw.start();
+    timerId = setInterval(updateClock, 200);
+}
+
+
+function stop(){
+    clearInterval(timerId);
+    sw.stop();
+    updateClock();
+}
+
+
+
+
+function getDateWorkingHoursTable() {
+    var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+    
+    var ret = "";    
+    ret += "<table>";
+    for (let [dateStr, workingHours] of DateToWorkingHoursMap){
+
+        var date = new Date(dateStr);
+        var month = months[date.getMonth()];
+        var day = days[date.getDay()];
+        
+        ret += "<tr>";
+        //console.log(key);
+        ret += "<td>";
+        ret += day + " | " + date.getDate() + " " + month + " " + date.getFullYear();
+        ret += "</td>";
+
+        ret += "<td>";
+        ret += " | " + workingHours
+        ret += "</td>";
+
+        ret += "</tr>";
+    }
+    ret += "</table>";
+    return ret;
+}
+
+
+function loadMap()
+{
+    var mapTemp = new Map();
+    for (var i = 0; i < localStorage.length; i++){
+        mapTemp.set(localStorage.key(i),
+                    localStorage.getItem(localStorage.key(i)));
+    }
+
+    var sortedMap = new Map([...mapTemp].sort());
+    DateToWorkingHoursMap = new Map([...sortedMap].reverse());
+    
+
+    for (let [key, value] of DateToWorkingHoursMap) {
+        console.log(key + ' = ' + value)
+    }    
+}
---
+++ js/stopwatch.js
@@ -0,0 +1,98 @@
+export class Stopwatch
+{
+    constructor()
+    {
+        this.timeElapsed = 2; //since start of timer
+        this.previousTime = 0;
+        this.timerId = null;
+
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 0;
+
+    }
+
+    getLengthAdjustedTimeUnitString(timeUnitStr)
+    {
+        console.log("length of timeunit: " + timeUnitStr.length);
+        if(1 == timeUnitStr.length)
+        {
+            return "0" + timeUnitStr;
+        }
+        return timeUnitStr;
+    }
+    
+
+    getTimeAsString()
+    {
+        return (this.getLengthAdjustedTimeUnitString(this.hours.toString()) + ":" +
+                this.getLengthAdjustedTimeUnitString(this.minutes.toString()) + ":" +
+                this.getLengthAdjustedTimeUnitString(this.seconds.toString()));
+    }
+
+    getHours()
+    {
+        return this.hours;
+    }
+
+    getMinutes()
+    {
+        return this.minutes;
+    }
+
+    getSeconds()
+    {
+        return this.seconds;
+    }
+
+    start()
+    {
+        clearInterval(this.timerId);
+        this.previousTime = Date.now();
+//        console.log(this.timeElapsed);
+        this.timerId = setInterval(this.update.bind(this), 200);
+    }
+
+    restart()
+    {
+        this.timeElapsed = 0;
+
+    }
+
+    stop(){
+        clearInterval(this.timerId);
+        this.update();
+    }
+    
+    pause()
+    {
+        clearInterval(this.timerId);
+        this.update();
+    }
+
+    resume()
+    {
+
+    }
+
+    setTime(timeElapsedInSeconds){
+        this.timeElapsed = timeElapsedInSeconds*1000;
+    }
+    
+    update()
+    {
+        var currentTime = Date.now();
+        this.timeElapsed = this.timeElapsed + currentTime - this.previousTime;
+        this.previousTime = currentTime;
+        
+        var timeElapsedInSeconds = Math.floor(this.timeElapsed/1000);
+        this.seconds = timeElapsedInSeconds%60;
+        this.minutes = Math.floor(timeElapsedInSeconds/60)%60;
+        this.hours = Math.floor(timeElapsedInSeconds/3600)%24;
+
+//        console.log(this.timeElapsed + currentTime - this.previousTime);
+//        console.log(this.getTimeAsString());
+    }
+
+
+}
